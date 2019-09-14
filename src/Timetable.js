5d39d2b99986ff937df3df7b732f45bd0dcc4148
Taru Airola <taru.airola@wunderdog.fi>
2019-09-14 22:10:55 +0300

    Lisää css-muotoilua aikatauluun

--- src/Timetable.js
+++ src/Timetable.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react'
+import './Timetable.css'
 
 class Timetable extends Component {
   render() {
     const { stopsData } = this.props
 
     return (
-      stopsData.map(stop =>
-        <div>
-          <h2>{stop.stopName}</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Kulkuväline</th>
-                <th>Lähtöaika</th>
-              </tr>
-            </thead>
-            <tbody>
-              {(stop.stopTimes.map(stopTime =>
-                <tr key={`${stopTime.route}-${stopTime.time}`}>
-                  <td>{stopTime.route}</td>
-                  <td>{stopTime.time}</td>
+      <div className="Timetable">
+        {stopsData.map(stop =>
+          <div className="Timetable_stop">
+            <h2>{stop.stopName}</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Kulkuväline</th>
+                  <th>Lähtöaika</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )
+              </thead>
+              <tbody>
+                {(stop.stopTimes.map(stopTime =>
+                  <tr key={`${stopTime.route}-${stopTime.time}`}>
+                    <td>{stopTime.route}</td>
+                    <td>{stopTime.time}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     )
   }
 }
