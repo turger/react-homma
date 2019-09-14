e7eae4fc7ff217b4f7f341bda353d288e8bf10bc
Taru Airola <taru.airola@wunderdog.fi>
2019-09-14 21:57:03 +0300

    Lisää useita pysäkkejä ja niille omat pysäkkiajat, jotka rendataan timetable-komponentissa

--- viisasten-kivi/src/App.js
+++ viisasten-kivi/src/App.js
@@ -3,29 +3,55 @@ import Timetable from './Timetable'
 import './App.css'
 
 function App() {
-  const stopTimes = [
+  const stopsData = [
     {
-      route: 'Bussi 123',
-      time: '12:55',
+      stopName: 'Itäväylän pysäkki',
+      stopTimes: [
+        {
+          route: 'Bussi 123',
+          time: '12:55',
+        },
+        {
+          route: 'Metro M1',
+          time: '12:58',
+        },
+        {
+          route: 'Metro M1',
+          time: '13:02',
+        },
+        {
+          route: 'Bussi 123',
+          time: '13:05',
+        }
+      ]
     },
     {
-      route: 'Metro M1',
-      time: '12:58',
-    },
-    {
-      route: 'Metro M1',
-      time: '13:02',
-    },
-    {
-      route: 'Bussi 123',
-      time: '13:05',
-    },
+      stopName: 'Joku toinen pysäkki',
+      stopTimes: [
+        {
+          route: 'Ratikka 3',
+          time: '12:55',
+        },
+        {
+          route: 'Bussi 55',
+          time: '12:58',
+        },
+        {
+          route: 'Ratikka 8',
+          time: '13:02',
+        },
+        {
+          route: 'Bussi 55',
+          time: '13:05',
+        }
+      ]
+    }
   ]
 
   return (
     <div className="App">
       <h1>Aikataulu</h1>
-      <Timetable stopTimes={stopTimes}/>
+      <Timetable stopsData={stopsData}/>
     </div>
   );
 }
