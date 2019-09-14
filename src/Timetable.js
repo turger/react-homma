67d16c78f39e9b7ad2cc2157dc12fe72f494438e
Taru Airola <taru.airola@wunderdog.fi>
2019-09-14 21:57:03 +0300

    Lisää useita pysäkkejä ja niille omat pysäkkiajat, jotka rendataan timetable-komponentissa

--- src/Timetable.js
+++ src/Timetable.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react'
 
 class Timetable extends Component {
   render() {
-    const { stopTimes } = this.props
+    const { stopsData } = this.props
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Kulkuväline</th>
-            <th>Lähtöaika</th>
-          </tr>
-        </thead>
-        <tbody>
-          {(stopTimes.map(stopTime =>
-            <tr key={`${stopTime.route}-${stopTime.time}`}>
-              <td>{stopTime.route}</td>
-              <td>{stopTime.time}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      stopsData.map(stop =>
+        <div>
+          <h2>{stop.stopName}</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Kulkuväline</th>
+                <th>Lähtöaika</th>
+              </tr>
+            </thead>
+            <tbody>
+              {(stop.stopTimes.map(stopTime =>
+                <tr key={`${stopTime.route}-${stopTime.time}`}>
+                  <td>{stopTime.route}</td>
+                  <td>{stopTime.time}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )
     )
   }
 }
