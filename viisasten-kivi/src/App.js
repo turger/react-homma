95dacd74225a7771597a757aae01f7ac5d1ea91e
Taru Airola <taru.airola@wunderdog.fi>
2019-09-14 23:24:59 +0300

    Lisää HSL apista haettu data timetable-komponenttiin

--- viisasten-kivi/src/App.js
+++ viisasten-kivi/src/App.js
@@ -16,55 +16,10 @@ class App extends Component {
   }
 
   render() {
-    const stopsData = [
-      {
-        stopName: 'Itäväylän pysäkki',
-        stopTimes: [
-          {
-            route: 'Bussi 123',
-            time: '12:55',
-          },
-          {
-            route: 'Metro M1',
-            time: '12:58',
-          },
-          {
-            route: 'Metro M1',
-            time: '13:02',
-          },
-          {
-            route: 'Bussi 123',
-            time: '13:05',
-          }
-        ]
-      },
-      {
-        stopName: 'Joku toinen pysäkki',
-        stopTimes: [
-          {
-            route: 'Ratikka 3',
-            time: '12:55',
-          },
-          {
-            route: 'Bussi 55',
-            time: '12:58',
-          },
-          {
-            route: 'Ratikka 8',
-            time: '13:02',
-          },
-          {
-            route: 'Bussi 55',
-            time: '13:05',
-          }
-        ]
-      }
-    ]
-
     return (
       <div className="App">
         <h1>Aikataulu</h1>
-        <Timetable stopsData={stopsData}/>
+        <Timetable stopsData={this.state.stopsData}/>
       </div>
     )
   }
