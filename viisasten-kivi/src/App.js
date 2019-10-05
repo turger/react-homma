46cd0db0bc122e5c234e199a7605de64e2fd5b75
Taru Airola <taru.airola@wunderdog.fi>
2019-10-05 23:41:33 +0300

    Kysely jolla haetaan viisi lähintä pysäkkiä syötetyn sijainnin perusteella

--- viisasten-kivi/src/App.js
+++ viisasten-kivi/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import Timetable from './Timetable'
 import Search from './Search'
 import './App.css'
-import {getStopsData, getLocationByName} from './requests'
+import {getStopsData, getStopsByRadius} from './requests'
 
 class App extends Component {
   constructor() {
@@ -19,19 +19,20 @@ class App extends Component {
     setInterval(async () => {
       await this.getStopsData()
     } , 60000)
-
-    const location = await getLocationByName('Mannerheimintie 1')
-    console.log('location', location)
   }
 
-  updateLocation = (lat, lon) => {
-    this.setState({lat, lon})
+  updateLocation = async (lat, lon) => {
+    this.setState({lat, lon}, this.getStopsData)
   }
 
   getStopsData = async () => {
-    const response = await getStopsData()
-    console.log('Response:', response) // open chrome devtools console panel Command+Option+J to see this output
-    this.setState({stopsData: response.data.stops})
+    const {lat, lon} = this.state
+    if (lat && lon) {
+      const stops = await getStopsByRadius(lat, lon)
+      const stopIds = stops.map(stop => stop.node.stop.gtfsId)
+      const response = await getStopsData(stopIds)
+      this.setState({stopsData: response.data.stops})
+    }
   }
 
   render() {
